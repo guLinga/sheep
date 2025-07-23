e41c12774305a62a603971e5dc0ddcc4dc45c168
guLinga <2634917964@qq.com>
2025-07-23 22:30:46 +0800

    feature: 增加任务信封模块

---
+++ scripts/task.js
@@ -0,0 +1,22 @@
+var pagination = require('hexo-pagination')
+
+// scripts/custom-routes.js
+hexo.extend.generator.register('task', function (locals) {
+  const config = this.config
+  let posts = locals.posts.sort(config.index_generator.order_by)
+
+  posts.data.sort((a, b) => (b.sticky || 0) - (a.sticky || 0))
+
+  posts.data = posts.data.filter((v) => v.index === 'task')
+
+  posts.length = posts.data.length
+
+  return pagination('/task', posts, {
+    perPage: config.index_generator.per_page,
+    format: 'task/%d%',
+    layout: 'task',
+    data: {
+      __index: true,
+    },
+  })
+})
